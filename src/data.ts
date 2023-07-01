d3b75cef675941140386d40a6ba91864266dd1ed
Tomas_Zagorskis <tomzagos@gmail.com>
2023-07-01 22:22:43 +0300

    feat: add mock data and type

---
+++ src/data.ts
@@ -0,0 +1,22 @@
+import { User } from './type';
+
+export const users: User[] = [
+	{
+		id: 'e648de87-7823-4a02-b736-58676fe0874a',
+		username: 'john_doe',
+		age: 25,
+		hobbies: ['reading', 'playing guitar'],
+	},
+	{
+		id: '731f2f16-5ab0-4d33-a8b2-57c3086e9c13',
+		username: 'jane_smith',
+		age: 30,
+		hobbies: ['painting', 'gardening', 'hiking'],
+	},
+	{
+		id: '77ef2b54-94cc-46f3-b434-6e6c3d41d524',
+		username: 'alex_wilson',
+		age: 28,
+		hobbies: ['cooking', 'photography'],
+	},
+];
