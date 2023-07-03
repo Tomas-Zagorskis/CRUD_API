bf5f59bd232c056c2fc92e21d4e10fa021908a94
Tomas Zagorskis <tomzagos@gmail.com>
2023-07-03 21:25:45 +0300

    refactor: move responses to file

--- src/controller.ts
+++ src/controller.ts
@@ -1,8 +1,14 @@
 import { IncomingMessage, ServerResponse } from 'http';
-import { validate } from 'uuid';
 
 import User from './model';
 import { getReqData, validateBody } from './utils';
+import {
+	getIdOrInvalidIdResponse,
+	getUserResponse,
+	invalidInputResponse,
+	serverErrorResponse,
+	userNotFoundResponse,
+} from './responses';
 
 const userModel = new User();
 
@@ -10,38 +16,26 @@ const userModel = new User();
 export async function getUsers(res: ServerResponse) {
 	try {
 		const users = await userModel.getUsers();
-		res.writeHead(200, { 'Content-Type': 'application/json' });
-		res.end(JSON.stringify(users));
+		getUserResponse(users, res);
 	} catch (error) {
-		console.log(error);
-		res.writeHead(500, { 'Content-Type': 'application/json' });
-		res.end(JSON.stringify({ message: 'Oops, something went wrong!' }));
+		serverErrorResponse(error as Error, res);
 	}
 }
 
 // GET api/users/:id
 export async function getUser(req: IncomingMessage, res: ServerResponse) {
 	try {
-		const id = req.url!.split('/')[3];
-
-		if (!validate(id)) {
-			res.writeHead(400, { 'Content-Type': 'application/json' });
-			res.end(JSON.stringify({ message: 'Invalid ID' }));
-		}
+		const id = getIdOrInvalidIdResponse(req, res);
 
 		const user = await userModel.getUser(id);
 
 		if (!user) {
-			res.writeHead(404, { 'Content-Type': 'application/json' });
-			res.end(JSON.stringify({ message: 'User not found' }));
+			userNotFoundResponse(res);
 		} else {
-			res.writeHead(200, { 'Content-Type': 'application/json' });
-			res.end(JSON.stringify(user));
+			getUserResponse(user, res);
 		}
 	} catch (error) {
-		console.log(error);
-		res.writeHead(500, { 'Content-Type': 'application/json' });
-		res.end(JSON.stringify({ message: 'Oops, something went wrong!' }));
+		serverErrorResponse(error as Error, res);
 	}
 }
 
@@ -51,77 +45,50 @@ export async function createUser(req: IncomingMessage, res: ServerResponse) {
 		const userData = await getReqData(req);
 
 		if (!validateBody(userData)) {
-			res.writeHead(400, { 'Content-Type': 'application/json' });
-			res.end(
-				JSON.stringify({
-					message: "Required fields are missing or don't match the type",
-				}),
-			);
+			invalidInputResponse(res);
 		} else {
 			const newUser = await userModel.createUser(JSON.parse(userData));
 			res.writeHead(201, { 'Content-Type': 'application/json' });
 			res.end(JSON.stringify(newUser));
 		}
 	} catch (error) {
-		console.log(error);
-		res.writeHead(500, { 'Content-Type': 'application/json' });
-		res.end(JSON.stringify({ message: 'Oops, something went wrong!' }));
+		serverErrorResponse(error as Error, res);
 	}
 }
 
 // PUT api/users/:id
 export async function updateUser(req: IncomingMessage, res: ServerResponse) {
 	try {
-		const id = req.url!.split('/')[3];
-
-		if (!validate(id)) {
-			res.writeHead(400, { 'Content-Type': 'application/json' });
-			res.end(JSON.stringify({ message: 'Invalid ID' }));
-		}
+		const id = getIdOrInvalidIdResponse(req, res);
 
 		const user = await userModel.getUser(id);
 
 		if (!user) {
-			res.writeHead(404, { 'Content-Type': 'application/json' });
-			res.end(JSON.stringify({ message: 'User not found' }));
+			userNotFoundResponse(res);
 		} else {
 			const userData = await getReqData(req);
 			if (!validateBody(userData, true)) {
-				res.writeHead(400, { 'Content-Type': 'application/json' });
-				res.end(
-					JSON.stringify({
-						message: "Required fields are missing or don't match the type",
-					}),
-				);
+				invalidInputResponse(res);
 			} else {
 				const updatedUser = await userModel.updateUser(user, userData);
 
-				res.writeHead(200, { 'Content-Type': 'application/json' });
-				res.end(JSON.stringify(updatedUser));
+				getUserResponse(updatedUser, res);
 			}
 		}
 	} catch (error) {
-		console.log(error);
-		res.writeHead(500, { 'Content-Type': 'application/json' });
-		res.end(JSON.stringify({ message: 'Oops, something went wrong!' }));
+		serverErrorResponse(error as Error, res);
 	}
 }
 
 // DELETE api/users/:id
 export async function deleteUser(req: IncomingMessage, res: ServerResponse) {
 	try {
-		const id = req.url!.split('/')[3];
-
-		if (!validate(id)) {
-			res.writeHead(400, { 'Content-Type': 'application/json' });
-			res.end(JSON.stringify({ message: 'Invalid ID' }));
-		}
+		const id = getIdOrInvalidIdResponse(req, res);
 
 		const user = await userModel.getUser(id);
 
 		if (!user) {
-			res.writeHead(404, { 'Content-Type': 'application/json' });
-			res.end(JSON.stringify({ message: 'User not found' }));
+			userNotFoundResponse(res);
 		} else {
 			await userModel.deleteUser(id);
 
@@ -129,8 +96,6 @@ export async function deleteUser(req: IncomingMessage, res: ServerResponse) {
 			res.end();
 		}
 	} catch (error) {
-		console.log(error);
-		res.writeHead(500, { 'Content-Type': 'application/json' });
-		res.end(JSON.stringify({ message: 'Oops, something went wrong!' }));
+		serverErrorResponse(error as Error, res);
 	}
 }
